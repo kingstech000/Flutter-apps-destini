246c887721ddf6b1ddf35edcd1121dccbcc086c9
kingstech <kingstech0001@gmail.com>
2024-01-14 16:05:46 +0100

    made changes to storybrain

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+// ignore_for_file: prefer_const_constructors, annotate_overrides, library_private_types_in_public_api
+
+import 'package:destini/story_brain.dart';
+import 'package:flutter/material.dart';
+
+StoryBrain myStory = StoryBrain();
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  const Destini({super.key});
+
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({super.key});
+
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("images/background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                    myStory.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      myStory.nextStory(1);
+                    });
+                  },
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStatePropertyAll(Colors.red),
+                  ),
+                  child: Text(
+                    myStory.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                //TODO: Step 26 - Use a Flutter Visibility Widget to wrap this FlatButton.
+                //TODO: Step 28 - Set the "visible" property of the Visibility Widget to equal the output from the buttonShouldBeVisible() method in the storyBrain.
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      myStory.nextStory(2);
+                    });
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStatePropertyAll(Colors.blue)),
+                  child: Text(
+                    myStory.getChoice2(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+//TODO: Step 24 - Run the app and try to figure out what code you need to add to this file to make the story change when you press on the choice buttons.
+
+//TODO: Step 29 - Run the app and test it against the Story Outline to make sure you've completed all the steps. The code for the completed app can be found here: https://github.com/londonappbrewery/destini-challenge-completed/
---
+++ lib/story.dart
@@ -0,0 +1,8 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
+}
+
